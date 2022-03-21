adfebf2f30100871ecddfc9f4cb7ee0a8c25093f
ben <bmwatt.bw@gmail.com>
2022-03-21 21:48:22 +0000

    GettingMessagesInRealTime

--- UniversityMessagingSystem/Controllers/HomeController.cs
+++ UniversityMessagingSystem/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace UniversityMessagingSystem.Controllers
 
             if(user.Password != password) return RedirectToAction("Index");
 
-            return RedirectToAction("Index", "MessageCentre", new {user = user});
+            return RedirectToAction("Index", "MessageCentre", new {userId = user.Id});
         }
 
         public IActionResult SignUp()
--- UniversityMessagingSystem/Controllers/MessageCentreController.cs
+++ UniversityMessagingSystem/Controllers/MessageCentreController.cs
@@ -19,38 +19,66 @@ namespace UniversityMessagingSystem.Controllers
             _logger = logger;
         }
        
-        public IActionResult Index(User user)
+        public IActionResult Index(int userId)
         {
+            User user = _context.user.FirstOrDefault(i => i.Id == userId);
+
             return View(user);
         }
 
-        public void SendMessage(string toEmail)
+        public void SendMessage(string toEmail, string message, string fromEmail)
         {
             //var fromUser = _context.user.FirstOrDefault(i => i.Email == viewModel.fromEmail);
             var toUser = _context.user.FirstOrDefault(i => i.Email == toEmail);
 
-            
-
-            Message mess = new Message
+            if(toUser != null)
             {
-                toEmail = toEmail,
-                toUser = toUser.Id,
-                message = "abc", 
-                fromEmail = "",
-                sentAt = DateTime.Now
-            };
+                Message mess = new Message
+                {
+                    toEmail = toEmail,
+                    toUser = toUser.Id,
+                    message = message,
+                    fromEmail = fromEmail,
+                    sentAt = DateTime.Now
+                };
 
-            _context.messages.Add(mess);
-            _context.SaveChanges();
+                _context.messages.Add(mess);
+                _context.SaveChanges();
+            }
         }
 
-        public JsonResult GetMessages()
+        public JsonResult Messages(string fromEmail)
         {
-            IEnumerable<Message> mess = _context.messages.Where( i => i.sentAt == DateTime.Now).ToList();
+            var mess = _context.messages.Where(i => i.toEmail == fromEmail).GroupBy(i => i.fromEmail).Select(i => i.ToList().OrderByDescending(i => i.sentAt).LastOrDefault()).ToList();
 
             var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(mess);
 
             return Json(jsonString);
+        }
+
+        public JsonResult GetMessages(string messList, string fromEmail)
+        {
+            if (messList == null || messList == "[]") return Json("[]");
+
+            IEnumerable<Message> temp = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Message>>(messList);
+
+            var x = _context.messages.Where(i => i.toEmail == fromEmail).ToList();
+
+            var newJson = Newtonsoft.Json.JsonConvert.SerializeObject(x);
+
+            foreach (var y in temp)
+            {
+                var item = x.FirstOrDefault(i => i.Id == y.Id);
+
+                if(item != null)
+                {
+                    x.Remove(item);
+                }
+            }
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(x);
+
+            return Json(new { response = json, oldList = newJson});
         }
     }
 }
